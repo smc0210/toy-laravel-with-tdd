1ff521af500ed6b799c4b56b7650d32efd796c0d
wiz <wiz@siot.do>
2019-04-19 15:14:36 +0900

    Rendering Activity With Polymorphism

--- app/Project.php
+++ app/Project.php
@@ -61,6 +61,6 @@ class Project extends Model
      */
     public function activity()
     {
-        return $this->hasMany(Activity::class);
+        return $this->hasMany(Activity::class)->latest();
     }
 }
