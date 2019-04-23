da2486df9b36b2b51199b5100b60f73eccad2415
wiz <wiz@siot.do>
2019-04-23 15:45:58 +0900

    Layered Tests and Invitations

---
+++ tests/Feature/InvitationsTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Facades\Tests\Setup\ProjectFactory;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class InvitationsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function a_project_can_invite_a_user()
+    {
+        $project = ProjectFactory::create();
+
+        $project->invite($newUser = factory(User::class)->create());
+
+        $this->signIn($newUser);
+        $this->post(action('ProjectTasksController@store', $project), $task = ['body' => 'Foo task']);
+
+        $this->assertDatabaseHas('tasks', $task);
+    }
+}
